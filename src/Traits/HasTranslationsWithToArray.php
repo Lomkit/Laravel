8f7f0cdedfc21b787f834dfcf8801f94343979f8
Gautier <gautier.deleglise@gmail.com>
2021-04-30 19:12:00 +0200

    :sparkles: added translations with to array

---
+++ src/Traits/HasTranslationsWithToArray.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Traits;
+use Spatie\Translatable\HasTranslations;
+trait HasTranslationsWithToArray
+{
+    use HasTranslations;
+    /**
+     * Convert the model instance to an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $attributes = parent::toArray();
+        foreach ($this->getTranslatableAttributes() as $field) {
+            $attributes[$field] = $this->getTranslation($field);
+        }
+        return $attributes;
+    }
+}
