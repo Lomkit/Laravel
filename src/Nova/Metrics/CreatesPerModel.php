5b5472e6888b9e4aae8ae9f0282bcdac1d619a58
Gautier <gautier.deleglise@gmail.com>
2021-05-05 16:08:13 +0200

    :sparkles: creates per model metric

---
+++ src/Nova/Metrics/CreatesPerModel.php
@@ -0,0 +1,103 @@
+<?php
+
+
+namespace Lomkit\Laravel\Nova\Metrics;
+
+
+use Carbon\Carbon;
+use Illuminate\Container\Container;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Metrics\Partition;
+
+class CreatesPerModel extends Partition {
+
+    protected $to;
+    protected $from;
+
+    public function from($from) {
+        $this->from = $from;
+    }
+
+    public function to($to) {
+        $this->to = $to;
+    }
+
+    public function getFrom() {
+        return $this->from ?? Carbon::now()->subMonth();
+    }
+
+    public function getTo() {
+        return $this->from ?? Carbon::now();
+    }
+
+    /**
+     * Calculate the value of the metric.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return mixed
+     */
+    public function calculate(NovaRequest $request)
+    {
+        $results = [];
+
+        foreach ($this->getModels() as $model) {
+            $count = $model::whereBetween($model::CREATED_AT, $this->getFrom(), $this->getTo())
+                ->count();
+            if ($count > 0) {
+                $results[get_class(new $model)] = $count;
+            }
+        }
+
+        return $this->result($results);
+    }
+
+    /**
+     * Determine for how many minutes the metric should be cached.
+     *
+     * @return  \DateTimeInterface|\DateInterval|float|int
+     */
+    public function cacheFor()
+    {
+        return now()->addHours(20);
+    }
+
+    /**
+     * Get the URI key for the metric.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'creates-per-model';
+    }
+
+    protected function getModels() {
+        $models = collect(File::allFiles(app_path()))
+            ->map(function ($item) {
+                $path = $item->getRelativePathName();
+                $class = sprintf('\%s%s',
+                    Container::getInstance()->getNamespace(),
+                    strtr(substr($path, 0, strrpos($path, '.')), '/', '\\'));
+
+                return $class;
+            })
+            ->filter(function ($class) {
+                $valid = false;
+
+                if (class_exists($class)) {
+                    $reflection = new \ReflectionClass($class);
+                    $instanciated = new $class;
+                    $valid = $reflection->isSubclassOf(Model::class) &&
+                        !$reflection->isAbstract() &&
+                        $instanciated->timestamps !== false
+                    ;
+                }
+
+                return $valid;
+            });
+
+        return $models;
+    }
+}
